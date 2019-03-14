fcdeea23495fa3328b5bdb0d9d10fe46d287d6db
vashkovey <EVashkov@qulix.com>
2019-03-14 15:53:59 +0300

    minor fixes index.js

--- script/index.js
+++ script/index.js
@@ -1,13 +1,31 @@
+var body = document.body;
 var signInButton = document.getElementById('signIn');
 var startButton = document.querySelectorAll('.pricing-section__button')[0];
+// var srcroolbarWidth = body.offsetWidth - body.clientWidth;
+
+scrollBarWidth = function() {
+   var div = document.createElement('div');
+
+   try {
+       document.body.appendChild(div);
+       return div.offsetWidth - div.clientWidth;
+   }
+
+   finally {
+       div.remove();
+   }
+}
 
 //добавляет обработчик события на кнопку Sign In
 signInButton.addEventListener('click', showModal);
 startButton.addEventListener('click', showNotification);
 
 function showModal(e) {
-    document.body.classList.add('modal-open');
+    var modal = new ModalWindow();
+    console.log(scrollBarWidth());
+    body.style.paddingRight = scrollBarWidth() + 'px';
     modal.makeLayOut();
+    
 }
 
 function showNotification(e) {
@@ -16,15 +34,15 @@ function showNotification(e) {
 
 //функция для создания элемента. Принимает три параметра: 1)элемент 2)класс 3)куда вставить элемент
 function PopupElement(elementType) {
-
+    var notification = new Notification('error', 'ERROR', 3000);
+    notification.makeLayOut();
 }
 
 PopupElement.prototype.makeLayOut = function (className, parentNode) {
     var element = this.createNewElement('div', className + ' popup', null);
 
-    var closeButton = this.createNewElement('a', 'popup__button-close', element);
-    closeButton.href = '#';
-    closeButton.addEventListener('click', PopupElement.prototype.removeLayout, false)
+    var closeButton = this.createNewElement('div', 'popup__button-close', element);
+    closeButton.addEventListener('click', this.removeLayout, false)
 
     this.createNewElement('h2', className + '__title', element);
     this.createNewElement('div', className + '__content', element);
@@ -33,12 +51,7 @@ PopupElement.prototype.makeLayOut = function (className, parentNode) {
 }
 
 PopupElement.prototype.removeLayout = function (e) {
-   e.preventDefault();
-    console.log(this.parentNode);
-    console.dir(this.parentNode.parentNode);
-    this.parentNode.parentNode.remove();
     this.parentNode.remove();
-    
 }
 
 PopupElement.prototype.createNewElement = function (tagName, className, parentNode) {
@@ -48,9 +61,9 @@ PopupElement.prototype.createNewElement = function (tagName, className, parentNo
     return element;
 }
 
-
-function ModalWindow() {
-    
+function ModalWindow(title, content) {
+    this.title = title;
+    this.content = content;
 }
 
 ModalWindow.prototype = Object.create(PopupElement.prototype);
@@ -58,9 +71,14 @@ ModalWindow.prototype = Object.create(PopupElement.prototype);
 ModalWindow.prototype.makeLayOut = function () {
     var modalWrapper = this.createNewElement('div', 'modal-backdrop', document.body);
     PopupElement.prototype.makeLayOut.apply(this, ['modal-window', modalWrapper]);
+    body.classList.toggle('modal-open');
 }
 
-
+ModalWindow.prototype.removeLayout = function (e) {
+    this.parentNode.parentNode.remove();
+    PopupElement.prototype.removeLayout.apply(this);
+    body.classList.toggle('modal-open');
+}
 
 function Notification(type, title, message, delay) {
     this.type = type;
@@ -70,12 +88,3 @@ function Notification(type, title, message, delay) {
 }
 
 Notification.prototype = Object.create(PopupElement.prototype);
-
-var modal = new ModalWindow();
-var notification = new Notification('error', 'ERROR', 3000);
-
-console.log(modal.elementType);
-console.log(modal.buttonClose);
-
-console.log(notification.elementType);
-console.log(notification.buttonClose);
