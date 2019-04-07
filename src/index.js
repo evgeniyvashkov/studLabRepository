21abdfc224fa21a5394c839feda98ef45f7f91d4
zhekaprb <zheka_cool@mail.ru>
2019-04-07 22:18:05 +0300

    connect to redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store'
 
 import { App } from './components/App';
 import { HomePage } from './components/Home-page';
@@ -8,14 +10,15 @@ import { RegistrationPage } from './components/Registration-page';
 import { LicensePage } from './components/LicensePage';
 
 ReactDOM.render((
-    <BrowserRouter>
-        <App>
-            <Switch>
-                <Route exact path='/' component={HomePage} />
-                <Route path='/registration' component={RegistrationPage} />
-                <Route path='/license' component={LicensePage} />
-            </Switch>
-        </App>
-    </BrowserRouter>
-), document.getElementById('root')
-);
+    <Provider store={store}>
+        <BrowserRouter>
+            <App>
+                <Switch>
+                    <Route exact path='/' component={HomePage} />
+                    <Route path='/registration' component={RegistrationPage} />
+                    <Route path='/license' component={LicensePage} />
+                </Switch>
+            </App>
+        </BrowserRouter>
+    </Provider>
+), document.getElementById('root'));
