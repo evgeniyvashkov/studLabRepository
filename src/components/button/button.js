9f4f190d7d60e6238542fa1803bd13f607e7170b
Yauheni Vashkou <Yauheni.Vashkou@itechart-group.com>
2019-04-11 11:13:21 +0300

    refactor button component: add defauld styles and behavior

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import './button.scss'
 
-export const Button = ({ value, ...attrs }) => {
-    return (
-        <button {...attrs}>
-            {value}
-        </button>
+export const Button = ({
+    value, onClick, className, disabled, ...attrs
+}) => {
+
+    const classes = classNames(
+        className,
+        //if button disabled,  add class .disabled
+        { disabled }
     )
+    //cancel action if button disabled
+    const onClickAction = (e) => (disabled) ? e.preventDefault() : onClick(e);
+
+    //A button can be tag <a /> or tag <button />
+    const Tag = (attrs.href) ? 'a' : 'button';
+
+    return (
+        <Tag
+            {...attrs}
+            className={classes}
+            onClick={onClickAction}
+        >
+            {value}
+        </Tag>
+    )
+}
+
+Button.defaultProps = {
+    className: 'button',
+    value: 'default button',
+    disabled: false,
+    onClick: () => { }
 }
