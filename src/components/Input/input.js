3fc65d2d3021873fa5c5ae55432cd73ac5862a24
Yauheni Vashkou <Yauheni.Vashkou@itechart-group.com>
2019-04-11 11:26:19 +0300

    add default behavior to input component

--- src/components/Input/input.js
+++ src/components/Input/input.js
@@ -9,21 +9,28 @@ export const Input = ({
 
     const classes = classNames(
         'form__group-input',
-        { error },
+        //If we received error in props, add class .error 
+        { error }
     )
 
     return (
-        <div className='form__group'>
+        <div className="form__group">
             {label &&
                 <label htmlFor={id} className="form__group-label">{label}</label>
             }
 
-            <input className={classes} {...attrs}/>
+            <input name={id} id={id} className={classes} {...attrs} />
 
             {error &&
-                <span className='form__group-error'>{error}</span>
+                <span className="form__group-error">{error}</span>
             }
         </div>
     )
+}
+
+Input.defaultProps = {
+    className: '',
+    label: 'default label',
+    error: false
 }
 
